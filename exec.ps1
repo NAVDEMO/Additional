1fbe99b06ae5f7e0895f8c273f0ee7b808599773
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-02-01 14:23:21 +0100

    Create exec.ps1

---
+++ exec.ps1
@@ -0,0 +1,24 @@
+﻿param(
+    [string] $StorageAccountName,
+    [string] $StorageAccountKey,
+    [string] $container,
+    [string] $blobName
+)
+
+$BlobContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+$scriptFile = Join-Path $PSScriptRoot ([Guid]::NewGuid().ToString() + ".ps1")
+$outputFile = "$scriptFile.output"
+
+$ProgressPreference = "SilentlyContinue"
+
+Get-AzureStorageBlobContent -Context $blobContext -Container $container -Blob $blobName -Destination $scriptFile -Force | Out-Null
+
+try {
+    . $scriptFile
+    if (Test-Path -Path $outputFile) {
+        Set-AzureStorageBlobContent -File $outputFile -Context $blobContext -Container $container -Blob "$blobName.output" -Force | Out-Null
+    }
+} finally {
+    Remove-Item $scriptFile -Force -ErrorAction SilentlyContinue
+    Remove-Item $outputFile -Force -ErrorAction SilentlyContinue
+}
